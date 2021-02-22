ff4e6fe09eb1016f81b970e33327c5cd0f4f1a75
mykolakryvyi <78466731+mykolakryvyi@users.noreply.github.com>
2021-02-22 12:37:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,8 +15,7 @@ def twitter_api(person):
     """
     base_url = "https://api.twitter.com/"
 
-    bearer_token = "AAAAAAAAAAAAAAAAAAAAAIKvMwEAAAAAGfXtAizYuenmkYuQLZ4qCDL%2F3n8%3D0\
-Bqy4HA6TVWqzcYq7Nk9UK0slRiRyVZvlEBtpyowkdXOupLBNf"
+    bearer_token = ""
 
     search_url = '{}1.1/friends/list.json'.format(base_url)
 
